ce5e3f50bbde1b79e571e6beff7580a02ca403cf
payal-315 <itsmepayal123@gmail.com>
2024-10-24 03:06:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('giftBox').addEventListener('click', function() {
+    document.getElementById('balloons').style.display = 'block';
+    document.getElementById('birthdaySong').play();
+    this.style.right = '30%';
+});
